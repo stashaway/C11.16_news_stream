d08b7c31ca2b9d88af825f5217e13fda887e1ba8
Tammy S lau <tammyslau29@gmail.com>
2017-01-12 10:18:40 -0800

    Basic Drawer Nav

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /**
  * Created by sloan on 1/11/2017.
  */
+function openNav() {
+    document.getElementById("side_nav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("side_nav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
