53755f267a8da815cb48609e4607431ed8ba9be8
stashaway <sloan.tash@yahoo.com>
2017-01-13 18:57:18 -0800

    just starting

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /**
  * Created by sloan on 1/11/2017.
  */
+$('.grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+});
+
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 200
+});
