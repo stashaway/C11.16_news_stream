11fa29ed8776a496924eb04ef7d7c65896a8953f
Brian Ault <baultik@gmail.com>
2017-01-23 13:54:10 -0800

    adding full example youtube data

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 /**
  * Created by sloan on 1/11/2017.
  */
-/*
-function twitch() {
-    $.ajax({
-        url:"https://api.twitch.tv/kraken/streams/?limit=5",
-        headers:{
-            "Client-ID":"a6ftugoz7pwach29wsg5wrsn4gfpug"
-        },
-        success:function (data) {
-            console.log(data);
-        },
-        error:function (data,err) {
-            console.error(err);
-        }
-    });
-}
-
-twitch();*/
