cc4c378c684bd5efd63dd445baef21a81e373d1d
NomadPort <jasonwelch4172@yahoo.com>
2017-01-27 10:01:52 -0800

    fb auth test

--- jason/test.js
+++ jason/test.js
@@ -1,63 +1,47 @@
-var auth2=null;
-function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-    auth2 = gapi.auth2.init();
-    if (isUserLoggedIn()) {
-        var profile = auth2.currentUser.get().getBasicProfile();
-        console.log('ID: ' + profile.getId());
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
 
-        $('.profile').attr('src', profile.getImageUrl());
-    }
-    displayLogginState();
-}
-function onFailure(error) {
-    console.log(error);
-}
-function renderButton() {
-    gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-    });
-}
-function signOut() {
-    auth2.signOut().then(function () {
-        console.log('User signed out.')
-    });
-    $('#sign_out').toggle();
-    $('#my-signin2').toggle();
-}
-function isUserLoggedIn(){
-    if(auth2){
-        return auth2.isSignedIn.get()!=false;
-    }
-    return false;
-}
-function displayLogginState(){
-    if (isUserLoggedIn()) {
-        $('#sign_out').show();
-        $('#my-signin2').hide();
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        console.log('user is signed in')
     } else {
-        $('#sign_out').hide();
-        $('#my-signin2').show();
+        // No user is signed in.
+        console.log('user is not signed in')
     }
+});
+
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+var provider = new firebase.auth.GoogleAuthProvider();
+
+if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                     // this value to authenticate with your backend server, if
+                     // you have one. Use User.getToken() instead.
 }
-$(document).ready(function() {
-    if(isUserLoggedIn()){
-        renderButton();
-        $('#sign_out').click(signOut).hide();
-    }else{
-        $('#sign_out').click(signOut).show();
 
-    }
+firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+}).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+});
 
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+}, function(error) {
+    // An error happened.
 });
